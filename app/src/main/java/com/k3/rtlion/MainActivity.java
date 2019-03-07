5f9e455056fbbe88313117264f9e188fb2368671
KeyLo99 <keylo99official@gmail.com>
2019-03-07 16:40:28 +0300

    Fix img id and show splash

--- app/src/main/java/com/k3/rtlion/MainActivity.java
+++ app/src/main/java/com/k3/rtlion/MainActivity.java
@@ -1,13 +1,40 @@
 package com.k3.rtlion;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.Window;
 
 public class MainActivity extends Activity {
+    private void init(){
+        showSplash();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+    private void showSplash(){
+        try {
+            LayoutInflater layoutInflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            final Dialog splashDialog = new Dialog(this, android.R.style.Theme_Black_NoTitleBar_Fullscreen);
+            splashDialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+            splashDialog.setContentView(layoutInflater.inflate(R.layout.layout_splash, null));
+            splashDialog.setCancelable(false);
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    splashDialog.cancel();
+                }
+            }, 1000);
+            splashDialog.show();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 }
