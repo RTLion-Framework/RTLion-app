47ed362c66f7cc2e0573b9dccc8dd7f4d11138ed
KeyLo99 <keylo99official@gmail.com>
2019-03-10 00:55:42 +0300

    Try connect method added to MainPageFrag

--- app/src/main/java/com/k3/rtlion/MainPageFrag.java
+++ app/src/main/java/com/k3/rtlion/MainPageFrag.java
@@ -5,11 +5,16 @@ import android.app.Activity;
 import android.content.Context;
 import android.view.ViewGroup;
 
+import com.github.nkzawa.socketio.client.Socket;
+import com.github.nkzawa.socketio.client.IO;
+
+
 public class MainPageFrag {
 
     private Activity activity;
     private Context context;
     private ViewGroup viewGroup;
+    private Socket socket;
 
     public MainPageFrag(Activity activity, ViewGroup viewGroup){
         this.activity = activity;
@@ -18,6 +23,13 @@ public class MainPageFrag {
     }
 
     public void init(){
-        // initialize views
+
+    }
+    private void tryConnect(){
+        try {
+            socket = new IO.socket("http://192.168.1.36:8081");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
