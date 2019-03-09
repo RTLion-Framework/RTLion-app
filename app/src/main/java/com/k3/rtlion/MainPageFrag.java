47f6c83e10bc23de4129b10a70f97df64b48220b
KeyLo99 <keylo99official@gmail.com>
2019-03-10 01:32:00 +0300

    Remove SocketIO library

--- app/src/main/java/com/k3/rtlion/MainPageFrag.java
+++ app/src/main/java/com/k3/rtlion/MainPageFrag.java
@@ -4,12 +4,6 @@ package com.k3.rtlion;
 import android.app.Activity;
 import android.content.Context;
 import android.view.ViewGroup;
-import android.widget.Toast;
-
-import com.github.nkzawa.socketio.client.Socket;
-import com.github.nkzawa.socketio.client.IO;
-
-import java.net.URISyntaxException;
 
 
 public class MainPageFrag {
@@ -17,7 +11,6 @@ public class MainPageFrag {
     private Activity activity;
     private Context context;
     private ViewGroup viewGroup;
-    private Socket socket;
 
     public MainPageFrag(Activity activity, ViewGroup viewGroup){
         this.activity = activity;
@@ -26,18 +19,7 @@ public class MainPageFrag {
     }
 
     public void init(){
-        String uri = "http://192.168.1.36:8081";
-        String isConnected = String.valueOf(connectSocket(uri));
-        Toast.makeText(activity, isConnected, Toast.LENGTH_SHORT).show();
+
     }
-    private boolean connectSocket(String uri){
-        try {
-            socket = IO.socket(uri).connect();
-        }catch (URISyntaxException e) {
-            e.printStackTrace();
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        return socket.connected();
-    }
+
 }
