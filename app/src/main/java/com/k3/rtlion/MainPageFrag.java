0d18b509395d448533a85a38a27c186acf8458b1
KeyLo99 <keylo99official@gmail.com>
2019-03-12 23:24:04 +0300

    Set edtxHostAddr actions & tryConnect method added

--- app/src/main/java/com/k3/rtlion/MainPageFrag.java
+++ app/src/main/java/com/k3/rtlion/MainPageFrag.java
@@ -3,11 +3,16 @@ package com.k3.rtlion;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Typeface;
 import android.support.design.widget.TextInputLayout;
+import android.view.KeyEvent;
+import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -19,6 +24,7 @@ public class MainPageFrag {
     private TextInputLayout tilHostAddr;
     private EditText edtxHostAddr;
     private Button btnConnect;
+    private String hostAddr, portNum;
 
     public MainPageFrag(Activity activity, ViewGroup viewGroup){
         this.activity = activity;
@@ -35,8 +41,28 @@ public class MainPageFrag {
 
     public void initialize(){
         initViews();
-
+        edtxHostAddr.setOnEditorActionListener(new edtxHostAddr_onEditorAction());
+        btnConnect.setOnClickListener(new btnConnect_onClick());
+    }
+    private class edtxHostAddr_onEditorAction implements TextView.OnEditorActionListener{
+        @Override
+        public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+            if (i == EditorInfo.IME_ACTION_SEND) {
+                tryConnect();
+                return true;
+            }
+            return false;
+        }
+    }
+    private class btnConnect_onClick implements Button.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            tryConnect();
+        }
     }
 
+    private void tryConnect(){
+
+    }
 
 }
