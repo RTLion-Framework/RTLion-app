795750c9be249eb5ce01fafe1d587abe15e5b3c9
KeyLo99 <keylo99official@gmail.com>
2019-03-14 15:09:45 +0300

    Initializer methods added to JSInterface

--- app/src/main/java/com/k3/rtlion/JSInterface.java
+++ app/src/main/java/com/k3/rtlion/JSInterface.java
@@ -1,4 +1,20 @@
 package com.k3.rtlion;
 
+import android.app.Activity;
+import android.content.Context;
+import android.webkit.WebView;
+
 public class JSInterface {
+
+    private Activity activity;
+    private Context context;
+    private WebView webView;
+
+    public JSInterface(Activity activity){
+        this.activity = activity;
+        this.context = activity.getApplicationContext();
+    }
+    public void initWebView(WebView webView){
+        this.webView = webView;
+    }
 }
