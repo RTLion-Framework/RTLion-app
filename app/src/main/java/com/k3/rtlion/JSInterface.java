40d61a05c7ef3bb63e8a1f191e88cdf6bf0ee4ad
KeyLo99 <keylo99official@gmail.com>
2019-03-16 01:24:45 +0300

    createJSCommand method added for creating commands easily

--- app/src/main/java/com/k3/rtlion/JSInterface.java
+++ app/src/main/java/com/k3/rtlion/JSInterface.java
@@ -54,7 +54,28 @@ public class JSInterface {
     public void getServerInfo(String url){
         webView.loadUrl(url + "#serverInfo");
     }
-
+    private String createJSCommand(int index, Object[] params){
+        StringBuilder jsCommand = new StringBuilder();
+        String jsHead = "javascript:",
+                serverCmd = JSCommands.values()[index].getServerCmd(),
+                clientCmd = JSCommands.values()[index].getClientCmd();
+        jsCommand.append(jsHead);
+        jsCommand.append(jsInterfaceName);
+        jsCommand.append(".");
+        jsCommand.append(serverCmd);
+        jsCommand.append("(");
+        jsCommand.append(clientCmd);
+        jsCommand.append("(");
+        if (params != null){
+            for (int i = 0; i < params.length; i++){
+                jsCommand.append(String.valueOf(params[i]));
+                if (i != params.length - 1)
+                    jsCommand.append(",");
+            }
+        }
+        jsCommand.append("));");
+        return jsCommand.toString();
+    }
     @JavascriptInterface
     public void fetchServerInfo(String info){
         Toast.makeText(activity, info, Toast.LENGTH_SHORT).show();
