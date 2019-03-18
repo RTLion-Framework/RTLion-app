ade5e2f06afecf102dd065b81bc0ae41c050c0c4
KeyLo99 <keylo99official@gmail.com>
2019-03-18 16:06:21 +0300

    Get and update methods added for SQLite (host address)

--- app/src/main/java/com/k3/rtlion/HostDB.java
+++ app/src/main/java/com/k3/rtlion/HostDB.java
@@ -1,6 +1,8 @@
 package com.k3.rtlion;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -8,6 +10,10 @@ public class HostDB extends SQLiteOpenHelper {
     private static final String databaseName = "RTLion_db";
     public String tableName = "hosts_table", dataName = "hosts";
     private static final int ver = 1;
+    private SQLiteDatabase hostsDB;
+    private Cursor dbCursor;
+    private ContentValues contentValues;
+
     public HostDB (Context context){
         super(context, databaseName, null, ver);
     }
@@ -20,6 +26,33 @@ public class HostDB extends SQLiteOpenHelper {
         try {
             db.execSQL("DROP TABLE IF EXISTS " + tableName);
             onCreate(db);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    private String getHostAddr(Context context){
+        String hostAddr = null;
+        try {
+            hostsDB = getWritableDatabase();
+            String[] hostColumn = {dataName};
+            dbCursor = hostsDB.query(tableName, hostColumn, null, null, null, null, null);
+            while (dbCursor.moveToNext()) {
+                hostAddr = dbCursor.getString(dbCursor.getColumnIndex(dataName));
+            }
+            hostsDB.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return hostAddr;
+    }
+    public void updateHostAddr(Context context, String newData){
+        try {
+            hostsDB = getWritableDatabase();
+            hostsDB.execSQL("DELETE FROM " + tableName);
+            contentValues = new ContentValues();
+            contentValues.put(dataName, newData);
+            hostsDB.insertOrThrow(tableName, null, contentValues);
+            hostsDB.close();
         }catch(Exception e){
             e.printStackTrace();
         }
