0bd8e391aec01fc5688e572d4519a9db90d703a3
KeyLo99 <keylo99official@gmail.com>
2019-03-18 15:56:24 +0300

    Add sqlite db initializer functions

--- app/src/main/java/com/k3/rtlion/HostDB.java
+++ app/src/main/java/com/k3/rtlion/HostDB.java
@@ -1,4 +1,27 @@
 package com.k3.rtlion;
 
-public class HostDB {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class HostDB extends SQLiteOpenHelper {
+    private static final String databaseName = "RTLion_db";
+    public String tableName = "hosts_table", dataName = "hosts";
+    private static final int ver = 1;
+    public HostDB (Context context){
+        super(context, databaseName, null, ver);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        db.execSQL("CREATE TABLE " + tableName + " (" + dataName + " TEXT);");
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVer, int newVer){
+        try {
+            db.execSQL("DROP TABLE IF EXISTS " + tableName);
+            onCreate(db);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
