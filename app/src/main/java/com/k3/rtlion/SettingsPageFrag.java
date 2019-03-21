8dc4d7006507564eff8144ca690664e160aa30c7
KeyLo99 <keylo99official@gmail.com>
2019-03-22 00:45:49 +0300

    Initializer methods added to Settings Page

--- app/src/main/java/com/k3/rtlion/SettingsPageFrag.java
+++ app/src/main/java/com/k3/rtlion/SettingsPageFrag.java
@@ -1,4 +1,27 @@
 package com.k3.rtlion;
 
+import android.app.Activity;
+import android.content.Context;
+import android.view.ViewGroup;
+
 public class SettingsPageFrag {
+
+    private Activity activity;
+    private Context context;
+    private ViewGroup viewGroup;
+    private JSInterface jsInterface;
+
+    public SettingsPageFrag(Activity activity, ViewGroup viewGroup, JSInterface jsInterface){
+        this.activity = activity;
+        this.context = activity.getApplicationContext();
+        this.viewGroup = viewGroup;
+        this.jsInterface = jsInterface;
+    }
+
+    private void initViews(){
+
+    }
+    public void initialize(){
+        initViews();
+    }
 }
