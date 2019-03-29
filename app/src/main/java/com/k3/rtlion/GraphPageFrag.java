2f4e2b6d024eed230fb731fc1a8b6b3a59d810e8
KeyLo99 <keylo99official@gmail.com>
2019-03-29 17:40:11 +0300

    Add views to Graph fragment & c/p removeConWarning

--- app/src/main/java/com/k3/rtlion/GraphPageFrag.java
+++ app/src/main/java/com/k3/rtlion/GraphPageFrag.java
@@ -2,7 +2,9 @@ package com.k3.rtlion;
 
 import android.app.Activity;
 import android.content.Context;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class GraphPageFrag {
@@ -13,11 +15,25 @@ public class GraphPageFrag {
     private JSInterface jsInterface;
 
     private TextView txvGraphWarning;
+    private LinearLayout llGraph;
 
     public GraphPageFrag(Activity activity, ViewGroup viewGroup, JSInterface jsInterface){
         this.activity = activity;
         this.context = activity.getApplicationContext();
         this.viewGroup = viewGroup;
         this.jsInterface = jsInterface;
+    }
+    private void initViews(){
+        txvGraphWarning = viewGroup.findViewById(R.id.txvGraphWarning);
+        llGraph = viewGroup.findViewById(R.id.llGraph);
+    }
+    public void initialize(){
+        initViews();
+        txvGraphWarning.setVisibility(View.VISIBLE);
+        llGraph.setVisibility(View.GONE);
+    }
+    public void removeConWarning(){
+        txvGraphWarning.setVisibility(View.GONE);
+        llGraph.setVisibility(View.VISIBLE);
     }
 }
