e13e73df1de2736ea036ec9b7d74ab2ab925cc2f
KeyLo99 <keylo99official@gmail.com>
2019-03-29 17:31:02 +0300

    Initialization made in Graph fragment

--- app/src/main/java/com/k3/rtlion/GraphPageFrag.java
+++ app/src/main/java/com/k3/rtlion/GraphPageFrag.java
@@ -1,4 +1,23 @@
 package com.k3.rtlion;
 
+import android.app.Activity;
+import android.content.Context;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 public class GraphPageFrag {
+
+    private Activity activity;
+    private Context context;
+    private ViewGroup viewGroup;
+    private JSInterface jsInterface;
+
+    private TextView txvGraphWarning;
+
+    public GraphPageFrag(Activity activity, ViewGroup viewGroup, JSInterface jsInterface){
+        this.activity = activity;
+        this.context = activity.getApplicationContext();
+        this.viewGroup = viewGroup;
+        this.jsInterface = jsInterface;
+    }
 }
