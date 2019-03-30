83c9bd4f99aa44a6429934ae66b6904c9082f437
KeyLo99 <keylo99official@gmail.com>
2019-03-30 16:41:25 +0300

    Fix ImageBase64 conversion

--- app/src/main/java/com/k3/rtlion/ImageBase64.java
+++ app/src/main/java/com/k3/rtlion/ImageBase64.java
@@ -8,7 +8,7 @@ import android.util.Base64;
 public class ImageBase64 {
 
     public Bitmap getImage(String base64img){
-        byte[] decodedString = Base64.decode(base64img, Base64.DEFAULT);
-        return BitmapFactory.decodeByteArray(base64img, 0, base64img.length);
+        byte[] decodedBytes = Base64.decode(base64img, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
     }
 }
