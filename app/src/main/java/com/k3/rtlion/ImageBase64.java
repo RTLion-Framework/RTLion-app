260c1ef2d4e9224eb9737e72387ed08b0bd16ec6
KeyLo99 <keylo99official@gmail.com>
2019-03-30 17:29:51 +0300

    try-catch block -> getImage method

--- app/src/main/java/com/k3/rtlion/ImageBase64.java
+++ app/src/main/java/com/k3/rtlion/ImageBase64.java
@@ -8,7 +8,12 @@ import android.util.Base64;
 public class ImageBase64 {
 
     public Bitmap getImage(String base64img){
-        byte[] decodedBytes = Base64.decode(base64img, Base64.DEFAULT);
-        return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
+        try {
+            byte[] decodedBytes = Base64.decode(base64img, Base64.DEFAULT);
+            return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
     }
 }
