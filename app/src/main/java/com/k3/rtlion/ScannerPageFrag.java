219417d52653577c66e99fe040526fff17a13855
KeyLo99 <keylo99official@gmail.com>
2019-04-10 13:15:25 +0300

    Initializer and constructor methods added

--- app/src/main/java/com/k3/rtlion/ScannerPageFrag.java
+++ app/src/main/java/com/k3/rtlion/ScannerPageFrag.java
@@ -1,4 +1,28 @@
 package com.k3.rtlion;
 
+import android.app.Activity;
+import android.content.Context;
+import android.view.ViewGroup;
+
 public class ScannerPageFrag {
+
+    private Activity activity;
+    private Context context;
+    private ViewGroup viewGroup;
+    private JSInterface jsInterface;
+
+    public MainPageFrag(Activity activity, ViewGroup viewGroup, JSInterface jsInterface){
+        this.activity = activity;
+        this.context = activity.getApplicationContext();
+        this.viewGroup = viewGroup;
+        this.jsInterface = jsInterface;
+    }
+
+    private void initViews(){
+
+    }
+
+    public void initialize(){
+        initViews();
+    }
 }
