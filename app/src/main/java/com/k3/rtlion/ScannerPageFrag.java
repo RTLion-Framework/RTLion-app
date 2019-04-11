8608eee40f88d8be051df0c7c84849c1840cc8af
KeyLo99 <keylo99official@gmail.com>
2019-04-11 23:34:43 +0300

    Import consoleMessage

--- app/src/main/java/com/k3/rtlion/ScannerPageFrag.java
+++ app/src/main/java/com/k3/rtlion/ScannerPageFrag.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.ConsoleMessage;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
