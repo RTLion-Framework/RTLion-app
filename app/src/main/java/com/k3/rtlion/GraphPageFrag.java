f87fce1bdaac3bf6c3c4b9dc1419cf56c24abc42
KeyLo99 <keylo99official@gmail.com>
2019-04-16 22:57:40 +0300

    Remove UI thread from showGraph function

--- app/src/main/java/com/k3/rtlion/GraphPageFrag.java
+++ app/src/main/java/com/k3/rtlion/GraphPageFrag.java
@@ -124,14 +124,9 @@ public class GraphPageFrag {
                     Toast.LENGTH_SHORT).show();
         }
     }
-    public void showGraph(final int frequency){
-        activity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                edtxFreq.setText(String.valueOf(frequency));
-                btnFFTGraph.performClick();
-            }
-        });
+    public void showGraph(int frequency){
+        edtxFreq.setText(String.valueOf(frequency));
+        btnFFTGraph.performClick();
     }
     private void hideViews(boolean state){
         if (state){
