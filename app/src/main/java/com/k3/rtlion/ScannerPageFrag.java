24e182e1c610b0105b074b92745a6188126cd1ad
KeyLo99 <keylo99official@gmail.com>
2019-04-17 20:24:34 +0300

    Disable refresh action if framework is busy ~ Scanner

--- app/src/main/java/com/k3/rtlion/ScannerPageFrag.java
+++ app/src/main/java/com/k3/rtlion/ScannerPageFrag.java
@@ -214,8 +214,13 @@ public class ScannerPageFrag {
                 @Override
                 public void run() {
                     swpScanner.setRefreshing(false);
-                    ((SettingsPageFrag)uiObjects[2]).updatedSettings = null;
-                    ((SettingsPageFrag)uiObjects[2]).getArgsFromServer();
+                    if(!viewsHidden && !((GraphPageFrag)uiObjects[3]).viewsHidden) {
+                        ((SettingsPageFrag) uiObjects[2]).updatedSettings = null;
+                        ((SettingsPageFrag) uiObjects[2]).getArgsFromServer();
+                    }else{
+                        Toast.makeText(activity, context.getString(R.string.framework_busy),
+                                Toast.LENGTH_SHORT).show();
+                    }
                 }
             }, refreshDuration);
         }
